ce4dec8e75692dbe80a1b714a06a9286624dbd80
mari967 <mar.y.sol.240@gmail.com>
2023-05-20 03:59:01 -0300

    Lista de locations con cards desplegables

--- app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/map/locations_list/LocationListScreen.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/map/locations_list/LocationListScreen.kt
@@ -1,10 +1,15 @@
 package ar.edu.utn.frba.mobile.turistapp.ui.map.locations_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,11 +18,16 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,27 +46,46 @@ fun LocationListScreen(locations: List<Location>) {
 
 @Composable
 fun LocationList(locations: List<Location>) {
-    LazyColumn{
+    LazyColumn(modifier = Modifier.background(MaterialTheme.colorScheme.background).padding(8.dp)){
         item{
             Title(stringResource(R.string.locations))
             }
         items(locations) { location ->
             LocationCard(location)
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
 
 @Composable
 fun LocationCard(location: Location) {
-    Column() {
-        Row(modifier = Modifier.padding(vertical = 10.dp), verticalAlignment = Alignment.CenterVertically) {
-            Text(text = location.order.toString(), style = MaterialTheme.typography.titleMedium)
-            Text(text = location.name, style = MaterialTheme.typography.titleMedium)
-            Chip(text = location.proximityLabel)
-            Spacer(modifier = Modifier.weight(1f))
-            Text(text = location.proximityValue.toString() + "m", style = MaterialTheme.typography.bodyMedium)
+    val isExpanded = remember { mutableStateOf(false) }
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, MaterialTheme.colorScheme.onSurface, RoundedCornerShape(8.dp))
+            .clickable { isExpanded.value = !isExpanded.value } // toggles the expanded state on click
+    ) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(8.dp)
+        ) {
+            Row(modifier = Modifier.padding(vertical = 10.dp), verticalAlignment = Alignment.CenterVertically) {
+                Text(text = location.order.toString(), style = MaterialTheme.typography.titleMedium)
+                Text(text = location.name, style = MaterialTheme.typography.titleMedium)
+                Chip(text = location.proximityLabel)
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = location.proximityValue.toString() + "m", style = MaterialTheme.typography.bodyMedium)
+                smallPlayerButton()
+            }
+            // If the card is expanded, show the description
+            if (isExpanded.value) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = location.description,  style = MaterialTheme.typography.bodyMedium)
+            }
         }
-        Text(text = location.description,  style = MaterialTheme.typography.bodyMedium)
     }
 }
 
@@ -89,27 +118,38 @@ fun Chip(
     }
 }
 
+
+
+@Composable
+private fun smallPlayerButton() {
+    Icon(painter = painterResource(R.drawable.ic_play_circle), contentDescription = "Play")
+    //TODO: agregar comportamiento para que cambie a ícono de pausa
+}
+
+//********************** PREVIEWS **********************//
+
+val testLocation: Location = Location(
+    order = 1,
+    name = "Casa Rosada",
+    description = "La Casa Rosada es la sede del Poder Ejecutivo de la República Argentina. " +
+            "Está ubicada en el barrio de Monserrat de la Ciudad Autónoma de Buenos Aires, frente" +
+            " a la histórica Plaza de Mayo. Su denominación oficial es Casa de Gobierno.",
+    proximityLabel = "Cerca",
+    proximityValue = 10
+)
+
+val testLocationList = listOf(testLocation, testLocation, testLocation, testLocation, testLocation)
 @Composable
 @Preview
 private fun LocationCardPreview() {
-    val testLocation: Location = Location(
-        order = 1,
-        name = "Casa Rosada",
-        description = "La Casa Rosada es la sede del Poder Ejecutivo de la República Argentina. " +
-                "Está ubicada en el barrio de Monserrat de la Ciudad Autónoma de Buenos Aires, frente" +
-                " a la histórica Plaza de Mayo. Su denominación oficial es Casa de Gobierno.",
-        proximityLabel = "Cerca",
-        proximityValue = 10
-    )
+
     LocationCard(testLocation)
 }
 
 @Composable
 @Preview
-private fun smallPlayerButton() {
-    Icon(Icons.Rounded.PlayArrow, contentDescription = "Play")
+private fun LocationListPreview() {
+    LocationList(testLocationList)
 }
-
-
 
 
