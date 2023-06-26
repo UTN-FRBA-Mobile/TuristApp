9b06e2498e54d01e4a4f6732a2256e00a74ff4d4
Kevin Alan Furman <kevin.furman@pedidosya.com>
2023-06-26 18:26:35 -0300

    Se muestra favorito en la home

--- app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/home/ToursViewModel.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/home/ToursViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import ar.edu.utn.frba.mobile.turistapp.core.api.MockToursAPI
 import ar.edu.utn.frba.mobile.turistapp.core.api.ToursAPI
+import ar.edu.utn.frba.mobile.turistapp.core.repository.FavoritesRepository
 import kotlinx.coroutines.Dispatchers
 
 class ToursViewModel(private val toursAPI: ToursAPI = MockToursAPI()): ViewModel() {
@@ -13,6 +14,6 @@ class ToursViewModel(private val toursAPI: ToursAPI = MockToursAPI()): ViewModel
     }
     var favoriteTours = liveData(Dispatchers.IO) {
         emit(null)
-        emit(toursAPI.getFavoriteTours())
+        emit(FavoritesRepository().getFavorites())
     }
 }
