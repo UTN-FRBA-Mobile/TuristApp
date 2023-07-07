32d1c7782905038cbe1222db97edf340f7553731
Germán Mallet <germanmallet@gmail.com>
2023-07-07 14:56:55 -0300

    AudioBar progress

--- app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/audio_bar/AudioBar.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/turistapp/ui/audio_bar/AudioBar.kt
@@ -1,9 +1,10 @@
 package ar.edu.utn.frba.mobile.turistapp.ui.audio_bar
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,8 +15,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,22 +29,17 @@ import androidx.compose.ui.unit.dp
 import ar.edu.utn.frba.mobile.turistapp.R
 import ar.edu.utn.frba.mobile.turistapp.core.utils.AudioPlayer
 import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun AudioBar(audioPlayer: AudioPlayer) {
     val playingData = audioPlayer.playingData.collectAsState().value
-    var currentPosition = remember { mutableStateOf(0F) }.value
+    var currentPosition by remember { mutableStateOf(0F) }
 
-    if (playingData.playing) {
-        LaunchedEffect(Unit) {
-            while(true) {
-                currentPosition = audioPlayer.currentPosition()
-                delay(1.seconds / 30)
-            }
+    LaunchedEffect(playingData.playing) {
+        while(playingData.playing) {
+            currentPosition = audioPlayer.currentPosition()
+            delay(10)
         }
-    } else {
-        currentPosition = audioPlayer.currentPosition()
     }
 
     Row(
@@ -49,9 +47,29 @@ fun AudioBar(audioPlayer: AudioPlayer) {
             .fillMaxWidth()
             .height(48.dp)
             .background(Color.Black)
-            .padding(bottom = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(5.dp)
     ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(5.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxSize()
+            )
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxHeight()
+                    .fillMaxWidth(currentPosition)
+            )
+        }
+
         Button(
             onClick = { audioPlayer.alternate() },
             modifier = Modifier.padding(start = 16.dp)
@@ -62,30 +80,6 @@ fun AudioBar(audioPlayer: AudioPlayer) {
                 contentDescription = stringResource(id = if (playingData.playing) R.string.pause_audio else R.string.play_audio),
                 tint = Color.Unspecified,
                 modifier = Modifier.size(30.dp)
-            )
-        }
-
-        Spacer(
-            modifier = Modifier
-                .weight(1f)
-                .height(4.dp)
-                .align(Alignment.CenterVertically)
-                .background(Color.Gray)
-        )
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .height(4.dp)
-                .align(Alignment.CenterVertically)
-                .background(Color.Green)
-                .padding(end = 4.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White)
-                    .fillMaxWidth(currentPosition)
             )
         }
     }
